68189a31f54380222e32f4d227a2aec390107a42
Damon <nuu.iag@gmail.com>
2026-04-28 09:39:36 +0200

    test

--- hello world/main.py
+++ hello world/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
     return {"message": "Hello", "world": "!"}
@@ -11,3 +10,8 @@ def read_root():
 @app.get("/name/{name}")
 def greet_name(name: str):
     return {"message": f"Hello, {name}!"}
+
+@app.get("/calculate/{number}")
+def calculate_square(number: float):
+    result = number ** 2
+    return {"number": number, "square": result}
